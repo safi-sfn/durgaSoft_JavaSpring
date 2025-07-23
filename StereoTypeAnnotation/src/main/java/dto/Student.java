fa8afb1e38d0ce5066a34a0acf033ee9abd45f07
safi-sfn <mdsafiruddin24@gmail.com>
2025-07-23 19:46:23 +0530

    Student DTO

---
+++ StereoTypeAnnotation/src/main/java/dto/Student.java
@@ -0,0 +1,28 @@
+package dto;
+
+public class Student {
+	private String sId;
+	private String sName;
+	private String sAddr;
+	
+	public String getsId() {
+		return sId;
+	}
+	public void setsId(String sId) {
+		this.sId = sId;
+	}
+	public String getsName() {
+		return sName;
+	}
+	public void setsName(String sName) {
+		this.sName = sName;
+	}
+	public String getsAddr() {
+		return sAddr;
+	}
+	public void setsAddr(String sAddr) {
+		this.sAddr = sAddr;
+	}
+	
+	
+}
