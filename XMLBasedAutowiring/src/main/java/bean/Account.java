0f41021899e50c72375aea8c1fb0ced6773a74fe
safi-sfn <mdsafiruddin24@gmail.com>
2025-07-23 07:41:40 +0530

    feat(bean) - bean class is created

---
+++ XMLBasedAutowiring/src/main/java/bean/Account.java
@@ -0,0 +1,28 @@
+package bean;
+
+public class Account {
+	private String accId;
+	private String accName;
+	private String accType;
+	
+	public String getAccId() {
+		return accId;
+	}
+	public void setAccId(String accId) {
+		this.accId = accId;
+	}
+	public String getAccName() {
+		return accName;
+	}
+	public void setAccName(String accName) {
+		this.accName = accName;
+	}
+	public String getAccType() {
+		return accType;
+	}
+	public void setAccType(String accType) {
+		this.accType = accType;
+	}
+	
+	
+}
---
+++ XMLBasedAutowiring/src/main/java/bean/Address.java
@@ -0,0 +1,30 @@
+package bean;
+
+public class Address {
+	private String pin;
+	private String state;
+	private String city;
+	
+	public String getPin() {
+		return pin;
+	}
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+	
+	
+}
