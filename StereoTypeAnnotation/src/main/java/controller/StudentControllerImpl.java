51909bb44997f7f4d921a2ecd89b98a10bbdbe5e
safi-sfn <mdsafiruddin24@gmail.com>
2025-07-23 19:46:07 +0530

    Interfaces implementation class

---
+++ StereoTypeAnnotation/src/main/java/controller/StudentControllerImpl.java
@@ -0,0 +1,34 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+
+import dto.Student;
+
+@Controller("studentController")
+public class StudentControllerImpl implements IStudentController {
+
+	@Override
+	public String addStudent(Student student) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student searchStudent(String sId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateStudent(Student student) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteStudent(String sID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ StereoTypeAnnotation/src/main/java/service/StudentServiceImpl.java
@@ -0,0 +1,40 @@
+package service;
+
+import org.springframework.stereotype.Service;
+
+import dto.Student;
+
+@Service("studentService")
+public class StudentServiceImpl implements IStudentService {
+
+	@Override
+	public String addStudent(Student student) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student searchStudent(String sId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student getStudent(String sId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateStudent(Student student) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteStudent(String sID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
