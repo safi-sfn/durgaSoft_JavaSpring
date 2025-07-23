bcc885580a703cd1d474dcf87ef075ecf3fecc95
safi-sfn <mdsafiruddin24@gmail.com>
2025-07-23 07:42:07 +0530

    feat(main)- launch main app

---
+++ XMLBasedAutowiring/src/main/java/mainApp/LaunchApp.java
@@ -0,0 +1,17 @@
+package mainApp;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import bean.Employee;
+
+public class LaunchApp {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationConfig.xml");
+		Employee emp = (Employee) context.getBean("empBean");
+		emp.showEmpDetails();
+
+	}
+
+}
