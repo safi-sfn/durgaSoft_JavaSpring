f53500cefa60d0aa9e3ffd58b7d7ffee0cc4befa
safi-sfn <mdsafiruddin24@gmail.com>
2025-07-27 18:22:52 +0530

    feat(dao) : correcting DB insertion and returning status

--- StereoTypeAnnotation/src/main/java/dao/StudentDaoImpl.java
+++ StereoTypeAnnotation/src/main/java/dao/StudentDaoImpl.java
@@ -21,14 +21,14 @@ public class StudentDaoImpl implements IStudentDao {
 		String status = "";
 		try {
 			Connection con = dataSource.getConnection();
-			PreparedStatement pst = con.prepareStatement("Select * from MyDB where SID = ?");
+			PreparedStatement pst = con.prepareStatement("Select * from students where SID = ?");
 			pst.setString(1, student.getsId());
 			ResultSet rs = pst.executeQuery();
 			boolean b = rs.next();
 			if(b==true) {
 				status="existed";
 			}else {
-				pst = con.prepareStatement("insert into MyDB values(?,?,?)");
+				pst = con.prepareStatement("insert into students values(?,?,?)");
 				pst.setString(1, student.getsId());
 				pst.setString(2, student.getsName());
 				pst.setString(3, student.getsAddr());
@@ -43,7 +43,7 @@ public class StudentDaoImpl implements IStudentDao {
 			status="failure";
 			e.printStackTrace();
 		}
-		return null;
+		return status;
 	}
 
 	@Override
