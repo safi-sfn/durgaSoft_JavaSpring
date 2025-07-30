bca5eae8ca2959e71a530a4e442a95682593480f
safi-sfn <mdsafiruddin24@gmail.com>
2025-07-30 21:25:34 +0530

    feat(validate) : Validate with Properties file

--- BeanValidation/src/main/java/validator/EmployeeValidator.java
+++ BeanValidation/src/main/java/validator/EmployeeValidator.java
@@ -1,11 +1,22 @@
 package validator;
 
+import java.io.IOException;
+import java.util.Properties;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import beans.Employee;
 
 public class EmployeeValidator implements Validator {
+	
+	private Resource resource;
+	
+	public void setResource(Resource resource) {
+		this.resource = resource;
+	}
 
 	@Override
 	public boolean supports(Class<?> clazz) {
@@ -16,30 +27,52 @@ public class EmployeeValidator implements Validator {
 	@Override
 	public void validate(Object obj, Errors errors) {
 		
+		
+		try {
+			Properties prop = PropertiesLoaderUtils.loadProperties(resource);
+		
+		
 		Employee emp = (Employee) obj;
 
-		if (emp.getEid() == null || emp.getEid() == "") {
-			errors.rejectValue("eid", "error.eid.required", "User ID is required");
-		} 
-
-		if (emp.getEname() == null || emp.getEname().equals("")) {
-			errors.rejectValue("uname", "error.uname.required", "User Name is required");
+		if(emp.getEid() == null || emp.getEid() == "") {
+			errors.rejectValue("eid", "error.eid.empty", prop.getProperty("error.eid.empty"));
+		}else{
+				if(!emp.getEid().startsWith("DSS-")) {
+					errors.rejectValue("eid", "error.eid.invalid", prop.getProperty("error.eid.invalid"));
+				}
 		}
-
-		if (emp.getEsal() <= 0.0f) {
-			errors.rejectValue("esal", "error.esal.required", "User Salary invalid");
+		if(emp.getEname() == null || emp.getEname() == "") {
+			errors.rejectValue("ename", "error.ename.empty", prop.getProperty("error.ename.empty"));
 		}
-
-		if (emp.getEage() < 18) {
-			errors.rejectValue("eage", "error.eage.minage", "min age");
-		} 
-
-		if (emp.getEemail() == null || emp.getEemail().equals("")) {
-			errors.rejectValue("eemail", "error.eemail.required", "email is required");
+		
+		if(emp.getEsal() <= 0.0f) {
+			errors.rejectValue("esal", "error.esal.invalid", prop.getProperty("error.esal.invalid"));
 		}
-
-		if (emp.getEmobile() == null || emp.getEmobile().equals("")) {
-			errors.rejectValue("emobile", "error.emobile.required", "Mobile No. is required");
+		
+		if(emp.getEage() < 18) {
+			errors.rejectValue("eage", "error.eage.minage", prop.getProperty("error.eage.minage"));
+		}
+		else if(emp.getEage() > 30) {
+			errors.rejectValue("eage", "error.eage.maxage", prop.getProperty("error.eage.maxage"));
+		}
+		if(emp.getEemail() == null || emp.getEemail() == "") {
+			errors.rejectValue("eemail", "error.eemail.empty", prop.getProperty("error.eemail.empty"));
+		}else{
+				if (!emp.getEemail().endsWith("@durgasoft.com")) {
+					errors.rejectValue("eemail", "error.eemail.invalid", prop.getProperty("error.eemail.invalid"));
+				}
+		}
+		if(emp.getEmobile() == null || emp.getEmobile() == "") {
+				errors.rejectValue("emobile", "error.emobile.empty", prop.getProperty("error.emobile.empty"));
+		}else{
+				if (!emp.getEmobile().startsWith("91-")) {
+					errors.rejectValue("emobile", "error.emobile.invalid", prop.getProperty("error.emobile.invalid"));
+				}
+		}
+		
+		} catch (IOException e) {
+			
+			e.printStackTrace();
 		}
 
 	}
